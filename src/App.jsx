52865f1c4f96f5279fd19aa399326135e4f5b6ab
Matija Zuber <matijazuber273@gmail.com>
2026-02-27 09:56:22 +0100

    put the spread operator for cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import data from "./data.js"
 function App() {
         const travelElements = data.map((travel)=>{
           return (
-            <Entry key={travel.id} img={{ src:travel.img.src, alt:travel.img.alt }} title={travel.title} country={travel.country} googleMapsLink={travel.googleMapsLink } dates={ travel.dates} text={travel.text} />
+            <Entry
+              key={travel.id}
+              {...travel} />
         )
       })
 
